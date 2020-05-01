41f90894ca71b0755c72910e317e580793304ceb
Gerardo Cetzal <backupinit0@gmail.com>
2020-05-01 16:50:18 -0500

    add page

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script>
-	export let name;
+	import Home from './pages/home.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Home />
 </main>
 
 <style lang="scss" global>
